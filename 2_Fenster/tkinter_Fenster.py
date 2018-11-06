d1f773bbf502fa641847a2dea70a530dbc359795
Leif-Erik Kotsch <leif.kotsch@gmail.com>
2018-11-06 23:35:45 +0100

    Updated tkinter script; Deleted _WIKI_IMAGES folder;

--- 2_Fenster/tkinter_Fenster.py
+++ 2_Fenster/tkinter_Fenster.py
@@ -2,10 +2,10 @@
 
 # Einfaches Fenster mit Tkinter
 
-import Tkinter
+import tkinter
 
 # Das Fenster wird in die Variable root geschrieben
-root = Tkinter.Tk()
+root = tkinter.Tk()
 
 # Nun dem Fenster root einen Namen verpassen
 root.title("VHS Esslingen - raspikurs")
@@ -14,12 +14,12 @@ root.title("VHS Esslingen - raspikurs")
 root.geometry('600x300')
 
 # Anschließend noch ein Paar Labels (Schriftfelder) einfügen
-Tkinter.Label(root, text="Grafikfenster mit TKinter", fg="#0A116B").pack()
-Tkinter.Label(root, text="\n      Viel Erfolg\n      mit TKinter\n").pack()
+tkinter.Label(root, text="Grafikfenster mit TKinter", fg="#0A116B").pack()
+tkinter.Label(root, text="\n      Viel Erfolg\n      mit TKinter\n").pack()
 
 # Und das Foto nicht vergessen!
-foto = Tkinter.PhotoImage(file="logo.png")
-Tkinter.Label(root, image=foto).pack()
+foto = tkinter.PhotoImage(file="logo.png")
+tkinter.Label(root, image=foto).pack()
 
 
 #Nun das Fenster starten
